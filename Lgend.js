840019ae1a4a872d8c07483976e208bc30c2e2b8
Anurag Kumar <43597455+akumar152@users.noreply.github.com>
2024-09-23 23:15:52 +0530

    Update Lgend.js

--- Lgend.js
+++ Lgend.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
-
 const Legend = ({ data }) => {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}> {/* Changed to column direction */}
             {Object.entries(data).map(([label, color], index) => (
                 <div
                     key={index}
-                    style={{ display: 'flex', alignItems: 'center', gap: '8px' }}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '10px', // Gap between the color view and the text
+                    }}
                 >
-                    <span>{label}</span>
                     <div
                         style={{
                             width: '20px',
                             height: '20px',
                             backgroundColor: color,
                             borderRadius: '50%',
+                            flexShrink: 0, // Prevents shrinking of the color view
                         }}
                     ></div>
-                    
+                    <span>{label}</span>
                 </div>
             ))}
         </div>
